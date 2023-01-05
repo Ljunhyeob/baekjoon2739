4132940b085f3131f733b55f551217561b001896
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-05 14:03:59 +0900

    Add files via upload

---
+++ baekjoon2739/baekjoon2739/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  baekjoon2739
+//
+//  Created by 이준협 on 2023/01/04.
+//
+
+import Foundation
+let line = readLine()!
+
+var num = Int(line)!
+
+for i in 1...9{
+    print("\(num) * \(i) = \(num*i)")
+}
+
+
